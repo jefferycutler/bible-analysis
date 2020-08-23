17616000935d258f41439ae09281e0ca42f117e4
jefferycutler <jeff.cutler.ca@gmail.com>
2020-08-23 15:16:48 -0400

    Fixed .gitignore file on csv, had wrong extension

--- cnvsource/conv_xml2csv.py
+++ cnvsource/conv_xml2csv.py
@@ -1,8 +1,17 @@
+######################################################
+# conv_xml2csv.py
+#  Program to convert bible verse xml files to csv
+#   Source xml should be contain a structure like this
+#      <verseFile>
+#        <v b=book c=chapter# v=verse#>verse text</v>
+#
+
 import argparse
 import csv 
 import os.path
 import xml.etree.ElementTree as ET
 
+#############################################
 ## define our argument parser
 parser = argparse.ArgumentParser(description=
   'Conversion script to convert bible verses xml file to csv')
@@ -11,12 +20,13 @@ parser.add_argument('--infile',help='input xml file path with bible verses',
 parser.add_argument('--outfile',help='output csv file path', required=True)
 args = parser.parse_args()
 
+#############################################
 ## verify the input file really exists
 if not os.path.isfile(args.infile):
     print('!!!! PROBLEM - INPUT FILE NOT VALID !!!')
     exit(1)
-## not testing output file because half an hour on stockeroverflow is enough
 
+#############################################
 ## parse the input xml file
 tree=ET.parse(args.infile)
 root=tree.getroot()
