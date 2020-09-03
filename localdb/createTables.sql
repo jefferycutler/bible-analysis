2cf7aeec73cdde0de4031e7086d951c6c49a8860
jefferycutler <jeff.cutler.ca@gmail.com>
2020-09-02 21:22:06 -0400

    added views to sql

--- localdb/createTables.sql
+++ localdb/createTables.sql
@@ -20,11 +20,10 @@ CREATE TABLE bible_verse_sentiment (
     book varchar(20) NOT NULL,   
     chapter int NOT NULL,   
     verse int NOT NULL,   
-    vtext varchar(2000) DEFAULT NULL,   
-    sentiment_dt datetime not null,   
+    api_call_dt datetime not null,   
     sentiment_magnitude decimal(5,3),   
     sentiment_score decimal(5,3),  
-    PRIMARY KEY (trns_abbr,book,chapter,verse,sentiment_dt) 
+    PRIMARY KEY (trns_abbr,book,chapter,verse,api_call_dt) 
 ) ;
 
 ----------------------------------------------
@@ -34,10 +33,37 @@ CREATE TABLE bible_verse_entity (
   book varchar(20) NOT NULL,
   chapter int NOT NULL,
   verse int NOT NULL,
-  vtext varchar(2000) DEFAULT NULL,
-  entity_dt datetime not null,
+  api_call_dt datetime not null,
   entity_name varchar(100),
   entity_type varchar(100),
   entity_salience float, 
-  PRIMARY KEY (trns_abbr,book,chapter,verse,entity_dt)
+  PRIMARY KEY (trns_abbr,book,chapter,verse,api_call_dt)
 ) ;
+
+----------------------------------------------
+-- Views to show unmatched verses (no api calls)
+create or replace view bible_verse_no_entity as
+select bv.trns_abbr
+      ,bv.book
+      ,bv.chapter
+      ,bv.verse
+from bible_verse bv
+  left join bible_verse_entity bve
+    on (bv.trns_abbr = bve.trns_abbr 
+        and bv.book = bve.book
+        and bv.chapter = bve.chapter
+        and bv.verse = bve.verse )
+where bve.api_call_dt is null ;
+
+create or replace view bible_verse_no_sentiment as
+select bv.trns_abbr
+      ,bv.book
+      ,bv.chapter
+      ,bv.verse
+from bible_verse bv
+  left join bible_verse_sentiment bvs
+    on (bv.trns_abbr = bvs.trns_abbr 
+        and bv.book = bvs.book
+        and bv.chapter = bvs.chapter
+        and bv.verse = bvs.verse )
+where bvs.api_call_dt is null
